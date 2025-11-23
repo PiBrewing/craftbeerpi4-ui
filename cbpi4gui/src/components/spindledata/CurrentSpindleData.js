deb4007ea458f81267ad22cd7c887a500287c91e
avollkopf <alexander.vollkopf@gmail.com>
2025-11-23 18:23:46 +0100

    update version and reactivate automated ispindle update on current data tab

--- cbpi4gui/src/components/spindledata/CurrentSpindleData.js
+++ cbpi4gui/src/components/spindledata/CurrentSpindleData.js
@@ -141,13 +141,13 @@ const CurrentSpindleData = () => {
   }, []);
 
   // reload data every 60 seconds
-  //useEffect(() => {
-  //  const interval = setInterval(() => {
-  //    load();
-  //  }, 60000);
-  //
-  //  return () => clearInterval(interval);
-  //}, []);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      load();
+    }, 60000);
+  
+    return () => clearInterval(interval);
+  }, []);
 
   const load = () => {
     sqlapi.getrecentdata(days , (data) => {
